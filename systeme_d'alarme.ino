72b6abf689c6b775a96d8f9ebb3d408191cc7c99
farouk09 <38142069+farouk09@users.noreply.github.com>
2021-05-27 16:30:13 +0100

    Rename sys_d_alarme.ino to systeme_d'alarme.ino

---
+++ systeme_d'alarme.ino
@@ -0,0 +1,16 @@
+
+int cm1=4,cm2=7,ct1=A0,ct2=A1,alarme=9;
+void setup() 
+{
+pinMode(cm1,INPUT);
+pinMode(cm2,INPUT);
+pinMode(alarme,OUTPUT);  
+}
+
+void loop() 
+{  
+if(digitalRead(cm1)==1 || digitalRead(cm2)==1 || analogRead(ct2)>=102 || analogRead(ct1)>=102 )
+{
+  tone(alarme,500,1000);   tone(9,900,1000);
+}
+}
